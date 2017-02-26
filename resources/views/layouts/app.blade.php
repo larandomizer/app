9c5cf79e7b42be2d2dc5d615ee64b72161d2d547
Daniel LaBarge <daniel@bexarcreative.com>
2017-02-26 00:20:12 -0600

    Removed dependency on modernizr.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
 <html class="no-js" lang="{{ config('app.locale') }}">
   <head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700">
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>@yield('pageTitle')</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-touch-icon.png">
-    <link rel="stylesheet" href="{{ config('app.url') }}/css/app.css">
-    <script src="{{ config('app.url') }}/js/modernizr-2.8.3.min.js"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:700">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
   </head>
   <body>
     <!--[if lte IE 9]>
@@ -24,6 +23,7 @@
       window.Laravel = {};
       window.Laravel.csrfToken = '{{ csrf_token() }}';
     </script>
-    <script src="{{ config('app.url') }}/js/app.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
   </body>
 </html>
