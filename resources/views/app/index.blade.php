5fef466666e8c22dd92040e22d726de3c636cc85
Eoghan O'Brien <eoghan@eoghanobrien.com>
2017-03-01 12:40:46 -0600

    Add responsive layout updates

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -5,18 +5,18 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-sm-4">
             <stat name="connections" icon="account-multiple" :title="connections.length" subtitle="Connections" :menu-items="menus.connections"></stat>
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-4">
             <stat name="uptime" icon="clock" :title="uptimeLabel" subtitle="Server Uptime" :menu-items="menus.server"></stat>
         </div>
-        <div class="col-md-4">
+        <div class="col-sm-4">
             <stat name="prizes" icon="trophy-variant-outline" :title="prizesLabel" subtitle="Prizes Available" :menu-items="menus.prizes"></stat>
         </div>
     </div>
     <div class="row mt-4">
-        <div class="col-md-12">
+        <div class="col-12">
             <div class="card card-table">
                 <div class="card-header">Prize Pool</div>
                 <grid :columns="columns" :records="connections" :connection="connection"></grid>
--- resources/views/app/partials/nav.blade.php
+++ resources/views/app/partials/nav.blade.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-light navbar-toggleable-md header">
     <div class="container">
         <a class="navbar-brand" href="{{ config('app.url') }}">{{ config('app.name') }}</a>
-        <div class="navbar-nav justify-content-start">
+        <div class="navbar-nav navbar-nav-user justify-content-start">
             <user :connection="connection" :connected="connected"></user>
             <notifications :notifications="notifications" :connected="connected"></notifications>
         </div>
-        <div class="navbar-nav ml-auto">
+        <div class="navbar-nav navbar-nav-connection ml-auto">
             <connection :connection="connection" :connected="connected"></connection>
         </div>
     </div>
