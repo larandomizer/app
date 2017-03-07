452e7c10b0077fba5e45499416d8d51d44bb2d00
Daniel LaBarge <daniel@bexarcreative.com>
2017-03-06 21:45:24 -0600

    Fix #1: Resetting Prizes Should Reset Connection Types

--- app/Server/Commands/ResetPrizes.php
+++ app/Server/Commands/ResetPrizes.php
@@ -2,8 +2,10 @@
 
 namespace App\Server\Commands;
 
+use App\Server\Contracts\Connection;
 use App\Server\Entities\Command;
 use App\Server\Entities\Prizes;
+use App\Server\Messages\UpdateConnections;
 use App\Server\Messages\UpdatePrizes;
 
 class ResetPrizes extends Command
@@ -18,7 +20,13 @@ class ResetPrizes extends Command
         $prizes = $this->dispatcher()->prizes();
         $everyone = $this->dispatcher()->connections();
 
+        $everyone->types([CONNECTION::LOSER, CONNECTION::WINNER])
+            ->each(function ($connection) {
+                $connection->type(CONNECTION::PLAYER);
+            });
+
         return $this->dispatcher()
-            ->broadcast(new UpdatePrizes($prizes), $everyone);
+            ->broadcast(new UpdatePrizes($prizes), $everyone)
+            ->broadcast(new UpdateConnections($everyone), $everyone);
     }
 }
